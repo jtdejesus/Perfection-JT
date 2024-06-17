c205ad984f86356c5ce4da8b4ad8b50333cae691
jtdejesus <157234255+jtdejesus@users.noreply.github.com>
2024-06-17 01:00:57 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    var number= 8128;
+    if (number ==6 || number == 28 || number == 496 || number ==8128)
+    Console.WriteLine ("Perfect Number");
+    else
+    Console.WriteLine ("Not A Perfect Number");
+  }
+}
